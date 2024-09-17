7e34fa128d5eceb311eaab4d4de6d3042223c15c
BuiTung09 <buitung095112@gmail.com>
2024-09-17 21:06:31 +0700

    Buble Sort

---
+++ src/bublesort/BubleSort.java
@@ -0,0 +1,13 @@
+
+package bublesort;
+import java.util.Scanner;
+public class BubleSort {
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+        
+        int[] a = new int[n];
+        
+    }
+    
+}
